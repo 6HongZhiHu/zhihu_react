eee60a325b8aef5b755e38d638543a55ee377531
hongzhihu <1731650140@qq.com>
2020-10-20 10:31:57 +0800

    commit

--- react_hong/src/App.js
+++ react_hong/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
-import {Route,Switch} from "react-router-dom"
-import Login from "./pages/login/login"
-import Admin from "./pages/admin/admin"
+//import {Route,Switch} from "react-router-dom"
+// import Login from "./pages/login/login"
+// import Admin from "./pages/admin/admin"
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <Switch>
+        {/* <Switch>
           <Route path="/login" component={Login}></Route>
           <Route path="/admin" component={Admin}></Route>
-        </Switch>
+        </Switch> */}
       </div>
     )
   }
